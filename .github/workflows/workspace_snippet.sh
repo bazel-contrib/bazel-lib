b762bc6e2f5993d3c246abba664a636a84d1b54f
Alex Eagle <alex@aspect.dev>
2021-12-11 11:44:43 -0800

    fix: release archive needs a strip_prefix
    Also remove some dead code

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -5,8 +5,8 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
-TAG_NO_PREFIX=${TAG:1}
-SHA=$(git archive --format=tar --prefix=bazel-lib-${TAG_NO_PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+PREFIX="bazel-lib-${TAG:1}"
+SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
 
@@ -18,6 +18,7 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
     name = "aspect_bazel_lib",
     sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
     url = "https://github.com/aspect-build/bazel-lib/archive/${TAG}.tar.gz",
 )
 \`\`\`
