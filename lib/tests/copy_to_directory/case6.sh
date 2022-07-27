94203cf468728cd103318a71435c8ba88137618b
Greg Magolan <gmagolan@gmail.com>
2022-07-27 13:17:50 -0700

    feat: add include_prefixes to copy_to_directory

--- lib/tests/copy_to_directory/case6.sh
+++ lib/tests/copy_to_directory/case6.sh
@@ -8,7 +8,7 @@ cd $(dirname $TEST_BINARY)
 cd case_6
 
 # A path that should be in the directory we have in data[]
-path="f/f2/f"
+path="f/f2/f2"
 if [ ! -f "$path" ]; then
     echo >&2 "Expected $path to exist in runfiles"
     exit 1
