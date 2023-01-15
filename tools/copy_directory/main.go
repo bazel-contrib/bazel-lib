01f5c3e949903d0a1b38d5c684f7613e72dcf0cc
Greg Magolan <gmagolan@gmail.com>
2023-01-14 19:09:57 -0800

    chore: parallelize copies in copy_directory with go routines (#326)

--- tools/copy_directory/main.go
+++ tools/copy_directory/main.go
@@ -9,23 +9,17 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
-type copyFile struct {
-	Src      string
-	Dst      string
-	FileInfo fs.FileInfo
-}
-
-type copySet []copyFile
-type dirSet []string
 type pathSet map[string]bool
 
-var copyPaths = make(copySet, 0, 10*1024)
-var dstDirs = make(dirSet, 0, 1024)
 var srcPaths = pathSet{}
+var copyWaitGroup sync.WaitGroup
+var hardlink = false
+var verbose = false
 
-func scanDir(src string, dst string) error {
+func copyDir(src string, dst string) error {
 	// filepath.WalkDir walks the file tree rooted at root, calling fn for each file or directory in
 	// the tree, including root. See https://pkg.go.dev/path/filepath#WalkDir for more info.
 	return filepath.WalkDir(src, func(p string, dirEntry fs.DirEntry, err error) error {
@@ -42,8 +36,7 @@ func scanDir(src string, dst string) error {
 
 		if dirEntry.IsDir() {
 			srcPaths[src] = true
-			dstDirs = append(dstDirs, d)
-			return nil
+			return os.MkdirAll(d, os.ModePerm)
 		} else {
 			info, err := dirEntry.Info()
 			if err != nil {
@@ -69,31 +62,59 @@ func scanDir(src string, dst string) error {
 				}
 				if stat.IsDir() {
 					// symlink points to a directory
-					return scanDir(linkPath, d)
+					return copyDir(linkPath, d)
 				} else {
 					// symlink points to a regular file
-					copyPaths = append(copyPaths, copyFile{
-						Src:      linkPath,
-						Dst:      d,
-						FileInfo: stat,
-					})
+					copyWaitGroup.Add(1)
+					go copy(linkPath, d, stat)
 					return nil
 				}
 			}
 
 			// a regular file
-			copyPaths = append(copyPaths, copyFile{
-				Src:      p,
-				Dst:      d,
-				FileInfo: info,
-			})
+			copyWaitGroup.Add(1)
+			go copy(p, d, info)
 			return nil
 		}
 	})
 }
 
+func copy(src string, dst string, info fs.FileInfo) {
+	defer copyWaitGroup.Done()
+	if !info.Mode().IsRegular() {
+		log.Fatalf("%s is not a regular file", src)
+	}
+	if hardlink {
+		// hardlink this file
+		if verbose {
+			fmt.Printf("hardlink %v => %v\n", src, dst)
+		}
+		err := os.Link(src, dst)
+		if err != nil {
+			// fallback to copy
+			if verbose {
+				fmt.Printf("hardlink failed: %v\n", err)
+				fmt.Printf("copy (fallback) %v => %v\n", src, dst)
+			}
+			err = copyFile(src, dst)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		// copy this file
+		if verbose {
+			fmt.Printf("copy %v => %v\n", src, dst)
+		}
+		err := copyFile(src, dst)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 // From https://opensource.com/article/18/6/copying-files-go
-func copy(src string, dst string) error {
+func copyFile(src string, dst string) error {
 	source, err := os.Open(src)
 	if err != nil {
 		return err
@@ -140,9 +161,6 @@ func main() {
 	src := args[0]
 	dst := args[1]
 
-	hardlink := false
-	verbose := false
-
 	if len(args) > 2 {
 		for _, a := range os.Args[2:] {
 			if a == "--hardlink" {
@@ -153,51 +171,8 @@ func main() {
 		}
 	}
 
-	// Gather list of files to copy
-	if err := scanDir(src, dst); err != nil {
+	if err := copyDir(src, dst); err != nil {
 		log.Fatal(err)
 	}
-
-	// Create all destination directories
-	for _, d := range dstDirs {
-		err := os.MkdirAll(d, os.ModePerm)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Perform copies or create hardlinks
-	// TODO: split out into parallel go routines?
-	for _, c := range copyPaths {
-		if !c.FileInfo.Mode().IsRegular() {
-			log.Fatalf("%s is not a regular file", c.Src)
-		}
-		if hardlink {
-			// hardlink this file
-			if verbose {
-				fmt.Printf("hardlink %v => %v\n", c.Src, c.Dst)
-			}
-			err := os.Link(c.Src, c.Dst)
-			if err != nil {
-				// fallback to copy
-				if verbose {
-					fmt.Printf("hardlink failed: %v\n", err)
-					fmt.Printf("copy (fallback) %v => %v\n", c.Src, c.Dst)
-				}
-				err = copy(c.Src, c.Dst)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-		} else {
-			// copy this file
-			if verbose {
-				fmt.Printf("copy %v => %v\n", c.Src, c.Dst)
-			}
-			err := copy(c.Src, c.Dst)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	copyWaitGroup.Wait()
 }
