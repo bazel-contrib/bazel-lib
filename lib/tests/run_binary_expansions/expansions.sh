87ff85a769751f042ca651345e89f5f808407ace
Alex Eagle <alex@aspect.dev>
2024-01-05 18:09:43 -0800

    chore: fix a test on macos

--- lib/tests/run_binary_expansions/expansions.sh
+++ lib/tests/run_binary_expansions/expansions.sh
@@ -5,7 +5,7 @@ mkdir -p $(dirname $1)
 outfile=$1
 rm -f $outfile
 for each in $@; do
-  sanitized=${each/darwin/PLATFORM}
+  sanitized=${each/darwin_arm64/PLATFORM}
   sanitized=${sanitized/k8/PLATFORM}
   echo $sanitized >>$outfile
 done
