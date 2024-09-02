0ad4c78b1aa2ac0cb05b71e189ace2b4f66cd5ab
Greg Magolan <greg@aspect.dev>
2024-09-02 09:16:15 -0700

    chore: apply lint fixes

--- lib/tests/stamping/stamper.sh
+++ lib/tests/stamping/stamper.sh
@@ -6,8 +6,8 @@ set -o nounset
 # is another option, which requires Bash 4 for associative arrays.
 while IFS= read -r line; do
   read key value <<<"$line"
-  declare $key="$value"
+  declare "$key"="$value"
 done < <(cat "${BAZEL_STABLE_STATUS_FILE:-/dev/null}" "${BAZEL_VOLATILE_STATUS_FILE:-/dev/null}")
 
 # A real program would do something useful with the stamp info, like pass it to a linker.
-echo "${BUILD_USER:-unstamped}" >$1
+echo "${BUILD_USER:-unstamped}" >"$1"
