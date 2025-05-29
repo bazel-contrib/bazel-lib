bazel-contrib/bazel-lib

commit a02dcf6fd8ee50462850429be84bb7d1152d5264

refactor: delete tar macro/rule code (#1083)
* refactor: delete tar macro/rule code

It was split out into a separate module for discoverability.
Leave behind a re-export to make this a non-breaking change for users.

* chore: add missing WORKSPACE dep

* chore: bump tar.bzl to latest

lib/tar.bzl
@@ -1,183 +1,10 @@
-"""General-purpose rule to create tar archives.
-
-Unlike [pkg_tar from rules_pkg](https://github.com/bazelbuild/rules_pkg/blob/main/docs/latest.md#pkg_tar):
-
-- It does not depend on any Python interpreter setup
-- The "manifest" specification is a mature public API and uses a compact tabular format, fixing
-  https://github.com/bazelbuild/rules_pkg/pull/238
-- It doesn't rely custom program to produce the output, instead
-  we rely on the well-known C++ program called "tar".
-  Specifically, we use the BSD variant of tar since it provides a means
-  of controlling mtimes, uid, symlinks, etc.
-
-We also provide full control for tar'ring binaries including their runfiles.
-
-The `tar` binary is hermetic and fully statically-linked.
-It is fetched as a toolchain from https://github.com/aspect-build/bsdtar-prebuilt.
-
-## Examples
-
-See the [`tar` tests](/lib/tests/tar/BUILD.bazel) for examples of usage.
-
-## Mutating the tar contents
-
-The `mtree_spec` rule can be used to create an mtree manifest for the tar file.
-Then you can mutate that spec using `mtree_mutate` and feed the result
-as the `mtree` attribute of the `tar` rule.
-
-For example, to set the owner uid of files in the tar, you could:
-
-```starlark
-_TAR_SRCS = ["//some:files"]
-
-mtree_spec(
-    name = "mtree",
-    srcs = _TAR_SRCS,
-)
-
-mtree_mutate(
-    name = "change_owner",
-    mtree = ":mtree",
-    owner = "1000",
-)
-
-tar(
-    name = "tar",
-    srcs = _TAR_SRCS,
-    mtree = "change_owner",
-)
-```
-
-TODO:
-- Provide convenience for rules_pkg users to re-use or replace pkg_files trees
+"""Re-export of https://registry.bazel.build/modules/tar.bzl to avoid breaking change.
+TODO(3.0): delete
 """
 
-load("@bazel_skylib//lib:types.bzl", "types")
-load("//lib:expand_template.bzl", "expand_template")
-load("//lib:utils.bzl", "propagate_common_rule_attributes")
-load("//lib/private:tar.bzl", _mutate_mtree = "mtree_mutate", _tar = "tar", _tar_lib = "tar_lib")
+load("@tar.bzl//tar:mtree.bzl", _mtree_mutate = "mtree_mutate", _mtree_spec = "mtree_spec")
+load("@tar.bzl//tar:tar.bzl", _tar = "tar")
 
-mtree_spec = rule(
-    doc = "Create an mtree specification to map a directory hierarchy. See https://man.freebsd.org/cgi/man.cgi?mtree(8)",
-    implementation = _tar_lib.mtree_implementation,
-    attrs = _tar_lib.mtree_attrs,
-)
-
-tar_rule = _tar
-
-tar_lib = _tar_lib
-
-def tar(name, mtree = "auto", stamp = 0, **kwargs):
-    """Wrapper macro around [`tar_rule`](#tar_rule).
-
-    ### Options for mtree
-
-    mtree provides the "specification" or manifest of a tar file.
-    See https://man.freebsd.org/cgi/man.cgi?mtree(8)
-    Because BSD tar doesn't have a flag to set modification times to a constant,
-    we must always supply an mtree input to get reproducible builds.
-    See https://reproducible-builds.org/docs/archives/ for more explanation.
-
-    1. By default, mtree is "auto" which causes the macro to create an `mtree_spec` rule.
-
-    2. `mtree` may be supplied as an array literal of lines, e.g.
-
-    ```
-    mtree =[
-        "usr/bin uid=0 gid=0 mode=0755 type=dir",
-        "usr/bin/ls uid=0 gid=0 mode=0755 time=0 type=file content={}/a".format(package_name()),
-    ],
-    ```
-
-    For the format of a line, see "There are four types of lines in a specification" on the man page for BSD mtree,
-    https://man.freebsd.org/cgi/man.cgi?mtree(8)
-
-    3. `mtree` may be a label of a file containing the specification lines.
-
-    Args:
-        name: name of resulting `tar_rule`
-        mtree: "auto", or an array of specification lines, or a label of a file that contains the lines.
-            Subject to [$(location)](https://bazel.build/reference/be/make-variables#predefined_label_variables)
-            and ["Make variable"](https://bazel.build/reference/be/make-variables) substitution.
-        stamp: should mtree attribute be stamped
-        **kwargs: additional named parameters to pass to `tar_rule`
-    """
-    mtree_target = "{}_mtree".format(name)
-    if mtree == "auto":
-        mtree_spec(
-            name = mtree_target,
-            srcs = kwargs.get("srcs", []),
-            out = "{}.txt".format(mtree_target),
-            **propagate_common_rule_attributes(kwargs)
-        )
-    elif types.is_list(mtree):
-        expand_template(
-            name = mtree_target,
-            out = "{}.txt".format(mtree_target),
-            data = kwargs.get("srcs", []),
-            # Ensure there's a trailing newline, as bsdtar will ignore a last line without one
-            template = ["#mtree", "{content}", ""],
-            substitutions = {
-                # expand_template only expands strings in "substitutions" dict. Here
-                # we expand mtree and then replace the template with expanded mtree.
-                "{content}": "\n".join(mtree),
-            },
-            stamp = stamp,
-            **propagate_common_rule_attributes(kwargs)
-        )
-    else:
-        mtree_target = mtree
-
-    tar_rule(
-        name = name,
-        mtree = mtree_target,
-        **kwargs
-    )
-
-def mtree_mutate(
-        name,
-        mtree,
-        srcs = None,
-        preserve_symlinks = False,
-        strip_prefix = None,
-        package_dir = None,
-        mtime = None,
-        owner = None,
-        ownername = None,
-        awk_script = Label("@aspect_bazel_lib//lib/private:modify_mtree.awk"),
-        **kwargs):
-    """Modify metadata in an mtree file.
-
-    Args:
-        name: name of the target, output will be `[name].mtree`.
-        mtree: input mtree file, typically created by `mtree_spec`.
-        srcs: list of files to resolve symlinks for.
-        preserve_symlinks: `EXPERIMENTAL!` We may remove or change it at any point without further notice. Flag to determine whether to preserve symlinks in the tar.
-        strip_prefix: prefix to remove from all paths in the tar. Files and directories not under this prefix are dropped.
-        package_dir: directory prefix to add to all paths in the tar.
-        mtime: new modification time for all entries.
-        owner: new uid for all entries.
-        ownername: new uname for all entries.
-        awk_script: may be overridden to change the script containing the modification logic.
-        **kwargs: additional named parameters to genrule
-    """
-    if preserve_symlinks and not srcs:
-        fail("preserve_symlinks requires srcs to be set in order to resolve symlinks")
-
-    # Check if srcs is of type list
-    if srcs and not types.is_list(srcs):
-        srcs = [srcs]
-    _mutate_mtree(
-        name = name,
-        mtree = mtree,
-        srcs = srcs,
-        preserve_symlinks = preserve_symlinks,
-        strip_prefix = strip_prefix,
-        package_dir = package_dir,
-        mtime = str(mtime) if mtime else None,
-        owner = owner,
-        ownername = ownername,
-        awk_script = awk_script,
-        out = "{}.mtree".format(name),
-        **kwargs
-    )
+mtree_mutate = _mtree_mutate
+mtree_spec = _mtree_spec
+tar = _tar

BUILD.bazel
@@ -6,7 +6,6 @@ load("@bazel_skylib//rules:write_file.bzl", "write_file")
 load("@buildifier_prebuilt//:rules.bzl", "buildifier")
 load("@gazelle//:def.bzl", "DEFAULT_LANGUAGES", "gazelle", "gazelle_binary")
 load("//lib:diff_test.bzl", "diff_test")
-load("//lib:tar.bzl", "mtree_spec")
 load("//lib:testing.bzl", "assert_contains")
 load("//lib:write_source_files.bzl", "write_source_files")
 load("//lib:yq.bzl", "yq")
@@ -129,43 +128,6 @@ bzl_library(
     srcs = ["deps.bzl"],
     visibility = ["//visibility:public"],
     deps = ["@gazelle//:deps"],
-)
-
-# Test case for mtree_spec: Ensure that multiple entries at the root directory are handled correctly (bug #851)
-# See lib/tests/tar/BUILD.bazel for why this is here.
-write_file(
-    name = "tar_test13_main",
-    out = "13project/__main__.py",
-    content = ["__main__.py"],
-)
-
-write_file(
-    name = "tar_test13_bin",
-    out = "13project_bin",
-    content = ["project_bin"],
-)
-
-mtree_spec(
-    name = "tar_test13_mtree_unsorted",
-    srcs = [
-        ":tar_test13_bin",
-        ":tar_test13_main",
-    ],
-)
-
-# NOTE: On some systems, the mtree_spec output can have a different order.
-#       To make the test less brittle, we sort the mtree output and replace the BINDIR with a constant placeholder
-genrule(
-    name = "tar_test13_mtree",
-    srcs = [":tar_test13_mtree_unsorted"],
-    outs = ["actual13.mtree"],
-    cmd = "sort $< | sed 's#$(BINDIR)#{BINDIR}#' >$@",
-)
-
-diff_test(
-    name = "tar_test13",
-    file1 = "tar_test13_mtree",
-    file2 = "//lib/tests/tar:expected13.mtree",
 )
 
 # Place the .vale.ini file in bazel-bin so the relative path it contains

MODULE.bazel
@@ -13,6 +13,9 @@ bazel_dep(name = "bazel_skylib", version = "1.5.0")
 bazel_dep(name = "platforms", version = "0.0.10")
 bazel_dep(name = "stardoc", version = "0.6.2")
 
+# TODO(3.0): remove this back-compat
+bazel_dep(name = "tar.bzl", version = "0.2.1")
+
 bazel_lib_toolchains = use_extension("@aspect_bazel_lib//lib:extensions.bzl", "toolchains")
 bazel_lib_toolchains.copy_directory()
 bazel_lib_toolchains.copy_to_directory()

lib/BUILD.bazel
@@ -116,10 +116,8 @@ bzl_library(
     name = "tar",
     srcs = ["tar.bzl"],
     deps = [
-        "//lib:expand_template",
-        "//lib:utils",
-        "//lib/private:tar",
-        "@bazel_skylib//lib:types",
+        "@tar.bzl//tar:mtree",
+        "@tar.bzl//tar:tar",
     ],
 )
 

lib/private/BUILD.bazel
@@ -289,16 +289,6 @@ bzl_library(
 )
 
 bzl_library(
-    name = "tar",
-    srcs = ["tar.bzl"],
-    visibility = ["//lib:__subpackages__"],
-    deps = [
-        "@aspect_bazel_lib//lib:paths",
-        "@bazel_skylib//rules:common_settings",
-    ],
-)
-
-bzl_library(
     name = "bats",
     srcs = ["bats.bzl"],
     visibility = ["//lib:__subpackages__"],

lib/repositories.bzl
@@ -25,6 +25,12 @@ def aspect_bazel_lib_dependencies():
             "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.7.1/bazel-skylib-1.7.1.tar.gz",
         ],
     )
+    http_archive(
+        name = "tar.bzl",
+        sha256 = "8710443803496e1b9b5b66f56ae55aa586338cb09a4ddeb9bb3d6df4e6da44c7",
+        strip_prefix = "tar.bzl-0.2.0",
+        url = "https://github.com/alexeagle/tar.bzl/releases/download/v0.2.0/tar.bzl-v0.2.0.tar.gz",
+    )
 
 DEFAULT_JQ_REPOSITORY = "jq"
 DEFAULT_JQ_VERSION = _DEFAULT_JQ_VERSION

commit 3d9d158db6455942b176eecd52882c0f59f168fb

fix(release): missed a public API symbol that requires re-export
Broken by https://github.com/bazel-contrib/bazel-lib/pull/1083

lib/tar.bzl
@@ -1,10 +1,13 @@
 """Re-export of https://registry.bazel.build/modules/tar.bzl to avoid breaking change.
+https://github.com/bazel-contrib/bazel-lib/pull/1083 moved these symbols to tar.bzl
 TODO(3.0): delete
 """
 
 load("@tar.bzl//tar:mtree.bzl", _mtree_mutate = "mtree_mutate", _mtree_spec = "mtree_spec")
-load("@tar.bzl//tar:tar.bzl", _tar = "tar")
+load("@tar.bzl//tar:tar.bzl", _tar = "tar", _tar_lib = "tar_lib", _tar_rule = "tar_rule")
 
 mtree_mutate = _mtree_mutate
 mtree_spec = _mtree_spec
 tar = _tar
+tar_lib = _tar_lib
+tar_rule = _tar_rule