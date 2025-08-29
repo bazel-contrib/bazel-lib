6083a6da22d3ea527d5f8bb4d1fcae1011583796
Alex Eagle <alex@aspect.dev>
2025-08-29 15:11:11 -0700

    chore: avoid potential MAX_ARGS issue with xargs

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -44,9 +44,9 @@ gzip <$ARCHIVE_TMP >$ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 # Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
-docs="$(mktemp -d)"
-bazel --output_base="$docs" query --output=label 'kind("starlark_doc_extract rule", //...)' \
-    | xargs bazel --output_base="$docs" build --remote_download_regex='.*doc_extract\.binaryproto'
+docs="$(mktemp -d)"; targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets" --remote_download_regex='.*doc_extract\.binaryproto'
 tar --create --auto-compress \
     --directory "$(bazel --output_base="$docs" info bazel-bin)" \
     --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
